ebc8717b32224d226ec371e748bc630c32b76c6f
Samuel Santos <135053698+sabsfilho@users.noreply.github.com>
2024-08-08 15:32:33 -0300

    Update Control.cs

--- EC2SchedulerLib/Control.cs
+++ EC2SchedulerLib/Control.cs
@@ -4,16 +4,17 @@ namespace EC2SchedulerLib;
 
 public static class Control
 {
-    internal const string BUCKET_NAME_PCB_LIB = "pcb-lib";
-    internal const string KEY_PAIR_NAME = "PutCallSP";
-    internal const string SUBNET_ID = "subnet-22814f47";
-    internal const string SECURITY_GROUP_ID = "sg-63587e05";
+    internal const string BUCKET_NAME_PCB_LIB = "SET_YOUR_BUCKET_NAME_PCB_LIB";
+    internal const string KEY_PAIR_NAME = "SET_YOUR_KEY_PAIR_NAME";
+    internal const string SUBNET_ID = "SET_YOUR_SUBNET_ID";
+    internal const string SECURITY_GROUP_ID = "SET_YOUR_SECURITY_GROUP_ID";
     internal const string INSTANCE_TYPE = "t3.medium";
     internal const string SCHEDULER_REQUESTED_BY_USER = "SchedulerRequestedByUser";
     internal const string SCHEDULER_KEY_START = "SchedulerStart";
     internal const string SCHEDULER_KEY_FINISH = "SchedulerFinish";
     internal const string SCHEDULER_KEY_IMAGE = "SchedulerImage";
     internal const int SCHEDULER_DEFAULT_DURATION_HOURS = 10;
+    
     /*
     input = SchedulerRequest
     Action = SchedulerEnum (required)
